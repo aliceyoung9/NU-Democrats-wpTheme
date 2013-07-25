f1f51480e08929d9247d85c744f98e92ee100d66
Alice the Guinea Pig <young.al@husky.neu.edu>
2013-07-25 13:55:49 -0400

    Ported over current theme.

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ */
+
+get_header(); ?>
+
+<h2>404; Page Not Found</h2>
+
+<p>If you've discovered a dead link, please 
+	<a href="mailto:neucollegedems+webadmin@gmail.com">email the web admin.</a></p>
+
+<?php get_footer(); ?>
