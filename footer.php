0a0676f63fd2914b668fa0032a481a949624019c
Alice the Guinea Pig <young.al@husky.neu.edu>
2013-07-25 14:00:57 -0400

    wiped files from other branch.

--- footer.php
+++
@@ -1,9 +0,0 @@
-  <nav>
-    <a href="http://www.facebook.com/NortheasternDems" target="_blank">Facebook</a> |
-    <a href="http://twitter.com/NUDemocrats" target="_blank">Twitter</a> | 
-    <a href="mailto:neucollegedems@gmail.com">Email the Officers</a>
-  </nav>
-
-</body>
-
-</html>
