090beaec431bdc9dd62d38e0f51eac4b9f1f51ac
Alice the Guinea Pig <young.al@husky.neu.edu>
2013-07-27 08:23:21 -0400

    Used the function to grab the title.

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <head>
     <link href='http://fonts.googleapis.com/css?family=Fauna+One|Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-    <title>Northeastern University College Democrats</title>
+    <title><?php bloginfo('name'); ?></title>
   </head>
 
 <body>
