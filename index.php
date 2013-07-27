b93d03c3fa03be0a27a07ea12626c6498402a788
Alice the Guinea Pig <young.al@husky.neu.edu>
2013-07-27 08:18:41 -0400

    Actually used the function for grabbing Site Title.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
-  <h2>Northeastern University College Democrats</h2>
-  
+    <h2><?php bloginfo('name'); ?></h2>
+
   <div class="sidenote">    //Site Under Construction <br />
                             //Wordpress is irritating.
   </div>
