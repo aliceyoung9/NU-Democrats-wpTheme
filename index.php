ff6234095ed0e24996dcb1e91c070803e2115815
Alice <young.al@husky.neu.edu>
2013-07-29 13:54:41 -0400

    widgit area moved to sidebar.php

--- index.php
+++ index.php
@@ -98,32 +98,8 @@
     </div>
 
     <footer>
-    <div class="container">
-        
-        <div class="row widget">
-            <div class="span3">
-              <h3>Title</h3>
-              <ul>
-                <li><a href="#">link</a></li>
-                <li><a href="#">link</a></li>
-                <li><a href="#">link</a></li>
-                <li><a href="#">link</a></li>
-                <li><a href="#">link</a></li>
-              </ul>
-            </div>
-            
-            <div class="span3">
-              <h3>world</h3>
-            </div>
-            <div class="span3">can you</div>
-            <div class="span3">
-              <h3>Connect</h3>
-                <div class="center">
-                <img src="<?php bloginfo('template_url'); ?>/img/facebook.png" class="social">
-                <img src="<?php bloginfo('template_url'); ?>/img/twitter.png" class="social">
-                </div>
-            </div>
-        </div>
+
+<?php get_sidebar(); ?>
 
 
 <?php get_footer(); ?>
---
+++ sidebar.php
@@ -0,0 +1,26 @@
+    <div class="container">
+        
+        <div class="row widget">
+            <div class="span3">
+              <h3>Title</h3>
+              <ul>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+                <li><a href="#">link</a></li>
+              </ul>
+            </div>
+            
+            <div class="span3">
+              <h3>world</h3>
+            </div>
+            <div class="span3">can you</div>
+            <div class="span3">
+              <h3>Connect</h3>
+                <div class="center">
+                <img src="<?php bloginfo('template_url'); ?>/img/facebook.png" class="social">
+                <img src="<?php bloginfo('template_url'); ?>/img/twitter.png" class="social">
+                </div>
+            </div>
+        </div>
