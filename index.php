f4dc2adfe19452eb7500eef89d59a7789c90d733
Alice <young.al@husky.neu.edu>
2013-07-29 12:51:19 -0400

    moved around things to make wp happy.

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
     <!-- Le styles -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="bootstrap.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
     
     </head>
 <body> 
