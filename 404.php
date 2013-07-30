520db7188d426ff74c9b81cf8b25c760f4551700
Alice the Guinea Pig <young.al@husky.neu.edu>
2013-07-30 16:21:23 -0400

    First attempt at 404 page.

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+    <div class="container content-wrap">
+    <div class="row">
+    <div class="span12">
+    <h2> we done goofed!</h2>
+
+    <p>If you've discovered a dead link, please 
+      <a href="mailto:neucollegedems+webadmin@gmail.com">email the web admin.</a></p>
+    </div>
+    </div> 
+    </div>
+
+    
+
+<?php get_sidebar(); ?>
+
+
+<?php get_footer(); ?>
