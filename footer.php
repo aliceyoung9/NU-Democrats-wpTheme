19e03a747f21cb8be3e0a82c82a2c689e04a7656
Alice the Guinea Pig <young.al@husky.neu.edu>
2013-07-30 15:33:31 -0400

    Carousel works again.

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="js/bootstrap.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
 
 </body>
 </html>
