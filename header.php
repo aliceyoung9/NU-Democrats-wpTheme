b5ae4a2cdb450da20b67ea7cc28fb3c4988b215e
Alice the Guinea Pig <young.al@husky.neu.edu>
2013-07-30 17:54:58 -0400

    added head hook, as is standard.

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
     <!-- Le styles -->
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/bootstrap.css" type="text/css" media="screen" />
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-    
+    <?php wp_head(); ?> 
 </head>
 <body> 
     <nav>
