f9998ede6f3225dd8a25093e397d25b0af335652
Alice the Guinea Pig <young.al@husky.neu.edu>
2013-07-31 01:09:20 -0400

    Finalized h1 for page title.

--- page.php
+++ page.php
@@ -4,7 +4,6 @@
     <div class="row">
     <div class="span12">
 
-    <h2><?php wp_title(); ?></h2>
     <h1><?php wp_title("",true); ?></h1>
 
     <p class="small">Last updated by <?php the_modified_author(); ?> on <?php the_modified_date(); ?></p>
