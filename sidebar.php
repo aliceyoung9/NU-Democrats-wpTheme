af99f520dc0fe7666e6ca08ff33885fe123fcce9
Alice the Guinea Pig <young.al@husky.neu.edu>
2013-08-06 17:59:06 -0400

    connect links link out.

--- sidebar.php
+++ sidebar.php
@@ -12,8 +12,10 @@
             <div class="span3">
               <h3>Connect</h3>
                 <div class="center">
-                <img src="<?php bloginfo('template_url'); ?>/img/facebook.png" class="social">
-                <img src="<?php bloginfo('template_url'); ?>/img/twitter.png" class="social">
+                <a href="http://www.facebook.com/NortheasternDems" target="_blank">
+                  <img src="<?php bloginfo('template_url'); ?>/img/facebook.png" class="social" alt="twitter logo"></a>
+                <a href="http://www.twitter.com/NUDemocrats" target="_blank">
+                <img src="<?php bloginfo('template_url'); ?>/img/twitter.png" class="social" alt="twitter logo"></a>
                 </div>
             </div>
         </div>
