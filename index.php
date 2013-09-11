1be302072b88aa6f30e4355ae81936a04118b1f2
Alice <young.al@husky.neu.edu>
2013-09-11 14:35:32 -0400

    and then I put up an announcement.

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
     <h2><?php bloginfo('name'); ?></h2>
 
   <div class="sidenote">    //Site Under Construction <br />
-                            //Wordpress is irritating.
+                            //Wordpress is irritating.<br />
+                            //First meeting is in Richards 300<br />
+                            //September 12<sup>th</sup>, 2013 at 7:30PM
   </div>
  
 <?php get_footer(); ?>
