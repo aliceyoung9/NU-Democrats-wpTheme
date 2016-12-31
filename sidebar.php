4a2ffe0793b3910ecddc634524122418328ec216
Alice Young <aliceyoung.9@gmail.com>
2016-12-31 05:50:59 -0500

    Remove obsolete center helping class.

--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,13 @@
 
             <div class="col-sm-3">
               <header>Connect</header>
-                <div class="center">
-                <a href="http://www.facebook.com/NortheasternDems" target="_blank">
-                  <img src="<?php bloginfo('template_url'); ?>/img/facebook.png" class="social" alt="facebook logo"></a>
-                <a href="http://www.twitter.com/NUDemocrats" target="_blank">
-                <img src="<?php bloginfo('template_url'); ?>/img/twitter.png" class="social" alt="twitter logo"></a>
+                <div class="text-xs-center">
+                  <a href="http://www.facebook.com/NortheasternDems" target="_blank">
+                    <img src="<?php bloginfo('template_url'); ?>/img/facebook.png" class="social" alt="facebook logo">
+                  </a>
+                  <a href="http://www.twitter.com/NUDemocrats" target="_blank">
+                    <img src="<?php bloginfo('template_url'); ?>/img/twitter.png" class="social" alt="twitter logo">
+                  </a>
                 </div>
             </div>
         </div>
