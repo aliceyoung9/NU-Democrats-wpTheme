8d4028d933f321509cab145074405c36c9aa66af
Alice Young <aliceyoung.9@gmail.com>
2016-12-30 22:47:41 -0500

    Dynamically display the copy date.

--- footer.php
+++ footer.php
@@ -1,27 +1,28 @@
-        <p class="pull-left copy">&copy; 2013 NU College Democrats</p>
-        <p class="pull-right copy">
-            Reluctantly made with Wordpress and Bootstrap.</p>
-        <div class="clear"></div>
-    </div>
-    </footer>
+    <p class="pull-left copy">
+      &copy; <?php echo date("Y"); ?> NU College Democrats
+    </p>
+    <p class="pull-right copy">
+      Reluctantly made with Wordpress and Bootstrap.</p>
+    <div class="clear"></div>
+  </div>
+  </footer>
 
+  <!-- JavaScript
+  ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="http://code.jquery.com/jquery.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
 
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-43695159-1', 'northeastern.edu');
+    ga('send', 'pageview');
 
-      ga('create', 'UA-43695159-1', 'northeastern.edu');
-      ga('send', 'pageview');
-
-    </script>
+  </script>
 
 </body>
 </html>
