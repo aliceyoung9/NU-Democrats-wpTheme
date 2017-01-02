6a2c542948c9cf9d21377b564345baa4d8609fa0
Alice Young <aliceyoung.9@gmail.com>
2017-01-02 17:48:48 -0500

    EBoard Page Template hides default content and shows values from ACF.

--- eboard.php
+++ eboard.php
@@ -6,11 +6,10 @@ Template Name: Executive Board Page
 
 <?php get_header(); ?>
 
-    <div class="container content-wrap eboard">
+  <div class="container content-wrap eboard">
     <div class="row">
     <div class="col-xs-12">
 
-    <?php if (is_page( 'Executive Board' )) : ?>
       <?php while (have_posts()) : the_post(); ?>
       <h1 id="post-<?php the_ID(); ?>"><?php the_title(); ?></h1>
 
@@ -26,7 +25,7 @@ Template Name: Executive Board Page
               <?php
               if ($image) {
                 echo
-                  '<div class="col-xs-2">',
+                  '<div class="col-sm-2">',
                   wp_get_attachment_image(
                     $image,
                     $size,
@@ -34,9 +33,9 @@ Template Name: Executive Board Page
                     array( "class" => "pro-pic" )
                   ),
                   '</div>',
-                  '<div class="col-xs-10">';
+                  '<div class="col-sm-10">';
               } else {
-                echo '<div class="col-xs-10 offset-xs-2">';
+                echo '<div class="col-sm-10 offset-sm-2">';
               }
               ?>
             <h3><?php the_sub_field('officer_name'); ?></h3>
@@ -44,40 +43,57 @@ Template Name: Executive Board Page
             <?php
               $major = get_sub_field('major');
               $hometown = get_sub_field('hometown');
+              $campaigns_worked_on = get_sub_field('campaigns_worked_on');
+              $favorite_democrat = get_sub_field('favorite_democrat');
+              $favorite_quote = get_sub_field('favorite_quote');
+              $quote_attribution = get_sub_field('quote_attribution');
 
               if ($major) {
-                echo '<strong>Major:</strong>', $major;
+                echo "<strong>Major: </strong>{$major}<br/>";
               }
 
               if ($hometown) {
-                echo '<strong>Hometown:</strong>', $hometown;
+                echo "<strong>Hometown: </strong>{$hometown}<br/>";
+              }
+
+              if ($campaigns_worked_on) {
+                echo "<strong>Campaigns worked on: </strong>{$campaigns_worked_on}<br/>";
+              }
+
+              if ($favorite_democrat) {
+                echo "<strong>Favorite Democrat: </strong>{$favorite_democrat}<br/>";
+              }
+
+              if ($favorite_quote) {
+                echo "<strong>Favorite Quote: </strong>
+                <p>
+                  <blockquote>“{$favorite_quote}”";
+
+                if ($quote_attribution) {
+                  echo "<footer>{$quote_attribution}</footer>";
+                }
+
+                echo "</blockquote></p>";
               }
              ?>
 
-            </div>
-          </div>
+             </div><!-- div class="col-sm-10 offset-sm-2" -->
+           </div><!--div class="row-padded" -->
           <?php
           endwhile;
         endif; ?>
 
-      <?php // the_content(''); ?>
-      <?php edit_post_link('Edit this Page'); ?>
-      <p class="small">Last updated by <?php the_modified_author(); ?>
-          on <?php the_modified_date(); ?></p>
-
-      </div></div>
       <?php endwhile; ?>
 
-    <?php else : ?>
-    <h1>Not Found</h1>
-    <p><?php _e("Sorry, but you are looking for something that isn't here."); ?></p>
-    <?php endif; ?>
+      <?php // the_content(''); ?>
+      <?php edit_post_link('Edit this Page'); ?>
+      <p class="small">
+        Last updated by <?php the_modified_author(); ?> on <?php the_modified_date(); ?>
+      </p>
 
-    </div>
-    </div>
-    </div>
-
-
+    </div> <!-- div class="col-xs-12" -->
+    </div> <!-- div class="row" -->
+  </div> <!-- <div class="container content-wrap eboard"-->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
