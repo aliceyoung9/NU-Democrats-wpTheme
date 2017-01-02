6a08ab5899d0dc0d05e7e1103b41c4a5540d61f9
Alice Young <aliceyoung.9@gmail.com>
2017-01-02 05:18:01 -0500

    Prepare for ACF-input Executive Board

--- eboard.php
+++ eboard.php
@@ -11,16 +11,62 @@ Template Name: Executive Board Page
     <div class="col-xs-12">
 
     <?php if (is_page( 'Executive Board' )) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-    <h1 id="post-<?php the_ID(); ?>"><?php the_title(); ?></h1>
+      <?php while (have_posts()) : the_post(); ?>
+      <h1 id="post-<?php the_ID(); ?>"><?php the_title(); ?></h1>
 
-    <?php the_content(''); ?>
-    <?php edit_post_link('Edit this Page'); ?>
-    <p class="small">Last updated by <?php the_modified_author(); ?>
-        on <?php the_modified_date(); ?></p>
+        <?php
+        if( have_rows('officers') ):
+          // loop through the rows of data
+          while ( have_rows('officers') ) : the_row();
 
-    </div></div>
-    <?php endwhile; ?>
+            $image = get_sub_field('pro_pic');
+            $size = 'medium'; // (thumbnail, medium, large, full or custom size)
+            ?>
+            <div class='row'>
+              <?php
+              if ($image) {
+                echo
+                  '<div class="col-xs-2">',
+                  wp_get_attachment_image(
+                    $image,
+                    $size,
+                    false,
+                    array( "class" => "pro-pic" )
+                  ),
+                  '</div>',
+                  '<div class="col-xs-10">';
+              } else {
+                echo '<div class="col-xs-10 offset-xs-2">';
+              }
+              ?>
+            <h3><?php the_sub_field('officer_name'); ?></h3>
+            <h3 class="position"><?php the_sub_field('office'); ?></h3>
+            <?php
+              $major = get_sub_field('major');
+              $hometown = get_sub_field('hometown');
+
+              if ($major) {
+                echo '<strong>Major:</strong>', $major;
+              }
+
+              if ($hometown) {
+                echo '<strong>Hometown:</strong>', $hometown;
+              }
+             ?>
+
+            </div>
+          </div>
+          <?php
+          endwhile;
+        endif; ?>
+
+      <?php // the_content(''); ?>
+      <?php edit_post_link('Edit this Page'); ?>
+      <p class="small">Last updated by <?php the_modified_author(); ?>
+          on <?php the_modified_date(); ?></p>
+
+      </div></div>
+      <?php endwhile; ?>
 
     <?php else : ?>
     <h1>Not Found</h1>
