04e0594c7acc176c51200a19b41c3f28a9da3eb4
Alice Young <aliceyoung.9@gmail.com>
2017-01-07 02:35:40 -0500

    Change whitespace in 404

--- 404.php
+++ 404.php
@@ -1,19 +1,18 @@
 <?php get_header(); ?>
 
-    <div class="container heavy-padded-bottom">
-    <div class="row">
-    <div class="span12">
-    <h2>Error 404; Page not Found</h2>
+<div class="container heavy-padded-bottom">
+  <div class="row">
+    <div class="col-xs-12">
+      <h2>Error 404; Page not Found</h2>
 
-    <p>If you've discovered a dead link, please 
-      <a href="mailto:neucollegedems+webadmin@gmail.com">email the web admin.</a></p>
+      <p>
+        If you've discovered a dead link, please
+        <a href="mailto:neucollegedems+webadmin@gmail.com">email the web admin.</a>
+      </p>
     </div>
-    </div> 
-    </div>
-
-    
+  </div>
+</div>
 
 <?php get_sidebar(); ?>
-
 
 <?php get_footer(); ?>
